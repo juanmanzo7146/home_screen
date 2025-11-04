ccbcf192ede96763f379cc26b04d061869ef2d3a
Juan Javier <juan.manzo7146@alumnos.udg.mx>
2025-11-03 19:07:26 -0600

    Home Screen, Imagen, Hello World e Icono

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
       debugShowCheckedModeBanner: false,
       title: 'Actividad -- Home Screen',
       home: Scaffold(
+        
         appBar: AppBar(
+          
           title: Padding(padding: const EdgeInsets.only(top: 30.0),
           child: Text('Bienvenidos al Catalogo de Peliculas'),
           
           ),
           centerTitle: true,
+          
         ),
-        body: Column(
+        body:  Column(
+          
         children: [
-           
+           DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('cinema.jpg'), fit: BoxFit.cover),
+          ),),
+          
            Image.asset('cinema.jpg',
            width: 700,
-           
+           alignment: AlignmentGeometry.bottomRight,
            ), 
-    
+            Text('Hellouuuu World', style: TextStyle(
+              color: Colors.blueAccent, 
+              fontSize: 50, 
+              fontWeight: FontWeight.bold
+              ),
+              ),
            Icon(
              Icons.movie, 
              size: 150.0,
              color: Colors.black,
+             
            ),
          ],
-        )
+        ),
       )
     );
 
